753fae447be0041a09057eb068c0dbd576f83cfd
Kirk Haines <wyhaines@gmail.com>
2023-01-27 07:14:17 -0700

    Add a little more documentation to Checksum.

--- src/base58.cr
+++ src/base58.cr
@@ -9,10 +9,14 @@ module Base58
   # There are two checksum variants that are supported.
   #
   # * [Base58Check](https://en.bitcoin.it/wiki/Base58Check_encoding) is the checksum algorithm used by Bitcoin. It combines an (optional) version/prefix to
-  # the front of the data payload, the first 4 characters of the SHA256 hash of the SHA256 has of the
-  # checksum + data combination before Base58 encoding.
+  # the front of the data payload with the data payload, and then an SHA256 hash is calculated from the combined prefix + payload. After that calculation,
+  # a second SHA256 hash is calculated from the first, and the first 4 bytes of that second hash are appended to the combined prefix + payload. The result
+  # is then Base58 encoded.
   #
-  # * [CB58](https://support.avax.network/en/articles/4587395-what-is-cb58)
+  # * [CB58](https://support.avax.network/en/articles/4587395-what-is-cb58) is the checksum algorithm used by Avalanche. It combines an (optional) version/prefix to
+  # the front of the data payload with the data payload, and then an SHA256 hash is calculated from the combined prefix + payload. After that calculation,
+  # the first 4 bytes of the hash are appended to the combined prefix + payload. The result is then Base58 encoded. i.e. it is essentialy Base58Check without
+  # the second SHA256 hash.
   #
   enum Checksum
     Base58Check
