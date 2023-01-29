5a5e13560c5b4058ad2ddd30105e467e1a82a3e6
Kirk Haines <wyhaines@gmail.com>
2023-01-29 00:02:03 -0700

    Add a touch of doc around the Array extension.

--- src/base58/extensions/array.cr
+++ src/base58/extensions/array.cr
@@ -1,4 +1,22 @@
 class Array
+  # This is a convenience method for creating a Slice from an Array. It is the equivalent
+  # of writing:
+  #
+  # ```
+  # Slice.new(array.size) { |idx| array[idx] }
+  # ```
+  #
+  # Use cases are primarily for convenience and readability.
+  #
+  # ```
+  # # In a spec, you need a Slice of 10 elements.
+  # [1, 2, 3, 5, 7, 11, 13, 17, 19, 23].to_slice
+  #
+  # # In a spec, you have a method that is returning an array, but your original
+  # # data is in a slice.
+  # do_something_and_get_an_array.to_slice.should eq original_slice
+  # ```
+  #
   def to_slice
     Slice.new(self.size) { |idx| self[idx] }
   end
