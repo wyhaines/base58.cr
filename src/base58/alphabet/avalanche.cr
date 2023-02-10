761b455bd2bd7f9dffd9900f7332b114fc10a3ed
Kirk Haines <wyhaines@gmail.com>
2023-02-09 17:44:20 -0700

    Fix a cut and paste error.

--- src/base58/alphabet/avalanche.cr
+++ src/base58/alphabet/avalanche.cr
@@ -3,7 +3,7 @@ require "./bitcoin"
 
 module Base58
   class Alphabet
-    # Polkadot, for its SS58 address format, uses the same alphabet as Bitcoin.
+    # Avalanche, for its SS58 address format, uses the same alphabet as Bitcoin.
     class Avalanche < Bitcoin
     end
   end
